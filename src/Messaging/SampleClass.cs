1258ac6efb56378fe274ee8da0e8303075d97a69
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-04-06 10:13:23 +0200

    created empty messaging class lib

---
+++ src/Messaging/SampleClass.cs
@@ -0,0 +1,10 @@
+﻿namespace Kurmann.Messaging;
+
+public class SampleClass
+{
+    public required string Id { get; set; }
+
+    public required string LastName { get; set; }
+
+    public string? FirstName { get; set; }
+}
